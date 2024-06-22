ab948241018a866d025c0f0f7093546acb676558
u005 <s.jeffery.ye@gmail.com>
2024-06-22 01:12:28 -0600

    expressjs aws rds connectivity established

--- express/server.js
+++ express/server.js
@@ -1,20 +1,39 @@
-// server/server.js
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import mysql from 'mysql2';
+import { dbConfig } from './config.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, '../build')));
+//el connectiono
+const connection = mysql.createConnection(dbConfig);
 
-// API endpoint
+connection.connect((err) => {
+    if (err) {
+        console.error('Connection failed: ', err);
+        return;
+    }
+    console.log('Connected to crumbdrop db');
+});
+
+// what is this lmao
+app.use(express.static(path.join(__dirname, '../src')));
+
+// db query !!
 app.get('/api/test', (req, res) => {
-    var bruh = ["piza"];
-    res.json(bruh);
-    console.log('success');
+    const query = 'SELECT * FROM your_table_name'; 
+    connection.query(query, (error, results) => {
+        if (error) {
+            res.status(500).send(error);
+            return;
+        }
+        res.json(results);
+    });
 });
 
 app.listen(PORT, () => {
